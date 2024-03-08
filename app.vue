cb3ae2a7ef0a06895b57b791ec6b588c1b6110d8
zeiadsalhin <zeyadali011@gmail.com>
2024-03-08 08:44:37 +0200

    v1

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="max-w-3xl px-4 mx-auto">
+
+    <header class="mt-8 mb-4 flex justify-between items-end ">
+      <div class="">
+        <h1 class="text-2xl font-semibold">Blog</h1>
+        <p class="text-gray-500">nuxt programming</p>
+      </div>
+      <Btn>Contact Me</Btn>
+    </header>
+
+    <!--Content-->
+
+    <NuxtPage />
+
+  </div>
+</template>
---
+++ components/content/Btn.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+    <button class="py-2 px-4 bg-white rouned-xl border my-2">
+        <slot></slot>
+    </button>
+</template>
+<style></style>
